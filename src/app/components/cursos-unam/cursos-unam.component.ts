c13c7c92cb16a377e9f21f73b3f9a7635f5e9c63
francisco-ag <francisamado80@gmail.com>
2023-08-28 23:22:56 -0600

    correcion

--- src/app/components/cursos-unam/cursos-unam.component.ts
+++ src/app/components/cursos-unam/cursos-unam.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { FormControl } from '@angular/forms';
 import { CursosService } from 'src/app/services/cursos.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCalificacionesComponent } from '../dialog-calificaciones/dialog-calificaciones.component';
 
 
 @Component({
@@ -13,12 +15,13 @@ export class CursosUnamComponent {
 
   dataSource = new MatTableDataSource<any[]>([]);
   
-  displayedColumns : string [] = ['id','alumno_fk',]
+  displayedColumns : string [] = ['id','alumno_fk','opciones']
 
   alumnos : any[] = []
 
   constructor(
-    private cursosService : CursosService
+    private cursosService : CursosService,
+    private dialog : MatDialog,
   ){ }
 
   ciclo_escolar = new FormControl('2023');
@@ -36,7 +39,7 @@ export class CursosUnamComponent {
       
       this.alumnos = alumnos
 
-      this.getCalificacionesAlumnos()
+      //this.getCalificacionesAlumnos()
 
 
     } catch (error) {
@@ -46,13 +49,24 @@ export class CursosUnamComponent {
   }
 
 
-  async getCalificacionesAlumnos(){
+  async getCalificacionesAlumnos(something : any){
     try {
 
-      
-      const calificaciones = await this.cursosService.getCalificacionesMateriasAlumnos(1,this.cicloEscolarSeleccionado );
+      console.log(something)
+
+      let id : number = parseInt(something.id_alumnos)
+      console.log(id)
+
+      const calificaciones = await this.cursosService.getCalificacionesMateriasAlumnos(id,this.cicloEscolarSeleccionado);
       console.log(calificaciones);
       
+      const dialog = this.dialog.open(DialogCalificacionesComponent,{
+        data:{
+          nombre : something,
+          alumno_calificaciones : calificaciones
+        }
+      })
+
       
 
     } catch (error) {
